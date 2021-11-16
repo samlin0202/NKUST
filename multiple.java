cbe6c9633928e46460087eb8ce171317bf527f97
samlin0202 <93572181+samlin0202@users.noreply.github.com>
2021-11-16 15:42:51 +0800

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,11 @@
+public class multiple {
+    public static void main(String[] args){
+        for(int i = 1;i <= 100;i++){
+            if(i % 3 ==0){
+                if(i % 7 ==0) {
+                    System.out.println(i);
+                }
+            }
+        }
+    }
+}
