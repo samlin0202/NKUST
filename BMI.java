3a09a897aee96dce810fa44c4ba32b47f6ec90bd
samlin0202 <93572181+samlin0202@users.noreply.github.com>
2021-11-16 15:38:27 +0800

    Add files via upload

---
+++ BMI.java
@@ -0,0 +1,9 @@
+public class BMI {
+    public static void main(String[] args) {
+        int W = 64;
+        float H = 1.78f , BMI;
+        BMI = W / ( H * H );
+        System.out.println(BMI);
+
+    }
+}
