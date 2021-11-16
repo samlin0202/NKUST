93a4de1bfcf5ed5b2d82e6790543734b3fff5999
samlin0202 <93572181+samlin0202@users.noreply.github.com>
2021-11-16 15:59:47 +0800

    Add files via upload

---
+++ AAA.java
@@ -0,0 +1,10 @@
+public class AAA {
+    public static void main(String[] args) {
+       int n=20;
+        for(int a = 1;a <= n;a++){
+            if (n % a == 0){
+                System.out.println(a);
+            }
+        }
+    }
+}
