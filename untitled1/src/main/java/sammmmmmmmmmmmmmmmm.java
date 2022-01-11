8ad9009a7779abf82db0e6d038e72d066e31bafc
C110181138 <93572159+C110181138@users.noreply.github.com>
2022-01-11 11:24:42 +0800

    Add files via upload
    更改過不會被鎖

---
+++ untitled1/src/main/java/sammmmmmmmmmmmmmmmm.java
@@ -0,0 +1,40 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+public class sammmmmmmmmmmmmmmmm{
+    public static void main(String[] args) throws IOException {
+        try {
+            int j = 1;
+            String URL_1 = "";
+            String URL_2 = "https://movie.douban.com/chart";
+            String URL_3 = "";
+            Document index_3 = Jsoup.connect(URL_2).get();
+            Element ououo = index_3.select("#douban-top250 div.douban-top250-hd h2 span a").first();
+            System.out.println(index_3.title());
+            System.out.println();
+            URL_3 = ououo.attr("href");
+            Document document = Jsoup.connect(URL_3).get();
+            Elements index_1 = document.select("div.article ol li div.pic a");
+
+            System.out.println(document.title());
+            System.out.println(ououo.attr("href"));
+            System.out.println();
+
+            for (Element item : index_1)
+            {
+                URL_1 = item.attr("href");
+                System.out.println("================================================");
+                System.out.println(item.attr("href"));
+                Document index_2 = Jsoup.connect(URL_1).get();
+
+                System.out.println("<電影名稱:"+index_2.title()+">");
+            }
+        } catch (Exception e) {
+            System.out.println("error" + e);
+        }
+    }
+}
